afb4e50da75d8ea405f39d9e78760624d1f07de7
joaovic499 <joaovictorsilva60@live.com>
2024-05-29 13:46:17 -0300

    Retaforação da cllass autentificação

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,23 +1,43 @@
-import { User } from './../entity/User';
 import { NextFunction, Response, Request } from "express"
 import jwt from 'jsonwebtoken'
 
 const secretKey = 'apiidopçekmjncxvvnmljo1993-0303@@@30230030@@@3030030'
 
 export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization')?.split(' ')[1];
-    if (token) {
-        jwt.verify(token, secretKey, (err, decoded) => {
-            if (err) {
-                console.error('Erro ao verificar o token JWT:', err);
-                return res.sendStatus(403);
-            }
-            req.user = decoded;
-            next();
+
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).json({
+            error: 'true',
+            message: 'Token não fornecido'
         });
-    } else {
-        console.error('Token JWT ausente na requisição');
-        res.sendStatus(401);
     }
-    
-};
+
+    const parts = authHeader.split(" ");
+        if (parts.length !== 2) {
+            return res.status(401).json({
+                error: 'true',
+                message: "Tipo de token invalido"
+            });
+        }
+
+    const [scheme, token] = parts;
+    if(scheme !== "Bearer"){
+        return res.status(401).json({
+            error: true,
+            message: "Tipo de token invalido"
+        });
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) =>{
+        if (err) {
+            return res.status(401).json({
+                error: true,
+                message:"Token invalido/ expirado"
+            });
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
