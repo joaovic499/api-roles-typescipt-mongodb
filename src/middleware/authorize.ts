71b4d1560dbb15a8948717225a2acf2cd7073708
joaovic499 <joaovictorsilva60@live.com>
2024-06-05 10:01:03 -0300

    Correção dos midleware autorization

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -1,18 +1,53 @@
 import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload} from 'jsonwebtoken';
+const secretKey = 'apiidopçekmjncxvvnmljo1993-0303@@@30230030@@@3030030'
 export const authorizeAdmin = (req: Request, res: Response, next: NextFunction) => {
-    if (req.user.role === 'admin') {
+    try{
+    const token = req.headers.authorization?.split(' ')['1'];
+    console.log(token);
+
+    if(!token){
+        return res.status(401).json({error: "Token não fornecido"})
+    }
+
+    const decoded = jwt.verify(token, secretKey) as JwtPayload;
+    const userRole = decoded.role;
+    console.log(userRole)
+    if (userRole === 'admin') {
         next();
     } else {
         res.sendStatus(403);
     }
-
+}catch(error){
+    if (error instanceof jwt.JsonWebTokenError){
+        return res.status(401).json({error: "Token inválido"});
+    }
+    console.error(error);
+    return res.status(500).json({ error: "Erro interno do servidor"});
+}
 };
 
 export const authorizeUser = (req: Request, res: Response, next: NextFunction) => {
-    if (req.user.role === 'user') {
-        next();
-    } else {
-        res.sendStatus(403);
-    }
+    try{
+        const token = req.headers.authorization?.split(' ')['1'];
+        console.log(token);
+
+        if(!token){
+            return res.status(401).json({error: "Token não fornecdo"})
+        }
+
+        const decoded = jwt.verify(token, secretKey) as JwtPayload;
+        const userRole = decoded.role;
+        console.log(userRole)
+        if(userRole === 'user') {
+            next();
+        } else {
+            res.sendStatus(403);
+        }
+    } catch(error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(500).json({ error: "Erro interno do servidor"});
+        }
+    };
 
 };
